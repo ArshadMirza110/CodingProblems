9d584d24dfda97ba2c5916f0890348d4139c4e85
Arshad Mirza <arshadmirza79@gmail.com>
2024-11-24 18:42:25 +0530

    Pattern Programs

---
+++ Patterns/TopLeftPattern.java
@@ -0,0 +1,19 @@
+public class Pattern {
+
+    public static void main(String[] args) {
+//#4: How to create a top left staircase Pattern
+        //      *
+        //     **
+        //    ***
+        //   ****
+        //  *****
+        // for(int i=1;i<=5;i++){
+        //     for (int j=1;j<=5-i;j++){ //1 se chalu aur N-row pe khatam
+        //         System.out.print(" ");
+        //     }
+        //     for (int k=1;k<=i;k++){// 1 se chalu aur row number ke count pe khatam
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+}
