667668b85667522eb8f84a3e498a7d8cbdcaf6ef
Arshad Mirza <arshadmirza79@gmail.com>
2024-11-25 23:45:44 +0530

    PrintEven

---
+++ DSA/PrintEven.java
@@ -0,0 +1,31 @@
+// Problem Description
+
+// Write a program that takes a positive integer N as input from the user and prints all natural numbers from 1 to N, with each number followed by a space (including the last number).
+
+
+// Problem Constraints
+// 1 <= N <= 1000000
+
+
+// Input Format
+// A single line representing N
+
+
+// Output Format
+// N space separated integers from 1 to N, with each number followed by a space, including the last number.
+
+import java.util.*;
+public class PrintEven {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i=1;i<=N;i++){
+            if (i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+
+
+    }
+}
